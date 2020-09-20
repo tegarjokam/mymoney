8054e8e6c811d3a583d23260ace8e76a81336ce9
tegarjokam <dtegarjokam354@gmail.com>
2020-09-21 06:29:31 +0900

    init

---
+++ src/main/java/com/mymoney/MymoneyApplication.java
@@ -0,0 +1,13 @@
+package com.mymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymoney/MymoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
